b0ba49839d1c8c28a0c260d3f334698e19125657
ibtechraj <raj_shk@rediffmail.com>
2020-06-09 09:39:58 +0530

    debug values

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,37 +1,36 @@
-import operate from './operate';
+import operate from "./operate";
 
 const calculate = (calculator, buttonName) => {
   let { result, nextVal, operation } = calculator;
 
-  const operBtns = ['+', '-', 'X', '÷'];
-  const numBtns = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const operBtns = ["+", "-", "X", "÷"];
+  const numBtns = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-  if (buttonName === 'AC') {
+  if (buttonName === "AC") {
     result = null;
     nextVal = null;
     operation = null;
   }
-  if (buttonName === '+/-') {
+
+  if (buttonName === "+/-") {
     result *= -1;
     nextVal *= -1;
   }
-  if (buttonName === '%') {
+
+  if (buttonName === "%") {
     result /= 100;
     nextVal /= 100;
   }
+
   if (numBtns.includes(buttonName)) {
     while (nextVal === null) {
-      nextVal = '';
+      nextVal = "";
     }
     nextVal += buttonName;
   }
-  if (buttonName === '.') {
-    if (!nextVal.includes('.')) {
-      nextVal += buttonName;
-    }
-  }
-  if (buttonName === '.') {
-    if (!nextVal.includes('.')) {
+
+  if (buttonName === ".") {
+    if (!nextVal.includes(".")) {
       nextVal += buttonName;
     }
   }
@@ -50,12 +49,15 @@ const calculate = (calculator, buttonName) => {
       operation = buttonName;
     }
   }
-  if (buttonName === '=') {
+
+  if (buttonName === "=") {
     while (nextVal !== null) {
       result = operate(result, nextVal, operation);
       nextVal = null;
     }
   }
+
+  console.log("6", nextVal, "7", result, "8", operation, "9", buttonName);
   return { result, nextVal, operation };
 };
 export default calculate;
