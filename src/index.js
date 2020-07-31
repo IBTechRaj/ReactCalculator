2b39b114a51d09d741bb0d7fd139cccb8c2cc57e
ibtechraj <raj_shk@rediffmail.com>
2020-07-31 11:39:28 +0530

    index.js errors cleared

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
 
-=======
-import App from './components/App';
->>>>>>> milestone-2
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
-<<<<<<< HEAD
-=======
-
->>>>>>> milestone-2
-=======
-import App from './components/App';
-import './components/index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> bbe161ce2e7dd2a37e51b358507a1bf4996c7328
+ReactDOM.render(<App />, document.getElementById("root"));
